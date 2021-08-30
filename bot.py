b8a3118d4bdf744a842850f1ad1671fa7ab13cf9
Egozey <65918692+egozey@users.noreply.github.com>
2021-08-30 18:31:30 +0300

    Update bot.py
    замена списка на словарь

--- bot.py
+++ bot.py
@@ -1,38 +1,35 @@
 import telebot
 import markups as m
 import parser
-TOKEN = ''
-task = []
+TOKEN = '1974316775:AAHnGEST2F6-ALoP1gGsnDgkG7HxkK_86Vc'
+task = {}
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
     chat_id = message.chat.id
+    task[chat_id] = []
     if message.text == "/start":
         msg = bot.send_message(chat_id ,"Введите мутацию для поиска")
         bot.register_next_step_handler(msg, askSource)
     elif message.text == "/help":
-        bot.send_message(chat_id, "не тупим", reply_markup=m.start_markup)
+        bot.send_message(chat_id, "Для поиска мутации на берите (или нажмите) /start", reply_markup=m.start_markup)
     else:
         bot.send_message(chat_id, "Я тебя не понимаю. Напиши /help.")
 
 def askSource(message):
-
-    task.append(message.text)
-
     chat_id = message.chat.id
+    task[chat_id].append(message.text)
     msg =bot.send_message(chat_id ,"Где искать", reply_markup=m.source_markup)
 
     bot.register_next_step_handler(msg, startPars)
 
 def startPars(message):
     chat_id = message.chat.id
-    task.append(message.text)
+    task[chat_id].append(message.text)
     print(task)
 
-    bot.send_message(chat_id ,parser.pars(task[1],task[0]), reply_markup=m.start_markup)
-    task.clear()
-
+    bot.send_message(chat_id ,parser.pars(task[chat_id][1],task[chat_id][0]), reply_markup=m.start_markup)
+    del(task[chat_id])
 
 
 
