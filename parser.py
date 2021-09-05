6e40c250041791cf262c5a8aaa8466c131e02911
Egozey <65918692+egozey@users.noreply.github.com>
2021-09-05 23:42:40 +0300

    Update parser.py
    добавлен парсер Clinvar

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 
@@ -18,14 +20,15 @@ def pars(gene,search):
 
     text = soup.text
     check = len(text)
-    
+
+
     out = ''
     if check < c[gene][1]:
         out += 'No results have been found that match your criteria.Please redefine your search criteria.'
-        return out
+        return '[+] LOVD\n\n'+out
     if check > 20000:
-        out += 'Слишком много информации конкретизируйте запрос'
-        return out
+        out += 'Too much information specify the request'
+        return '[+] LOVD\n\n'+out
     ch = c[gene][2]-1
 
     text_data = text.split('\n')[c[gene][5]:-17]
@@ -35,6 +38,50 @@ def pars(gene,search):
             out+= text_data[ch-c[gene][7]]+ '  '+ text_data[ch] + '\n'
             ch+=c[gene][6]
         except:
-            return out
+            return '[+] LOVD\n\n'+out
+
+def parsClinvar(gene,seach):
+    url = 'https://www.ncbi.nlm.nih.gov/clinvar/?term=({})+{}'.format(seach,gene)
+
+    response = requests.get(url)
+
+
+    if urlparse(response.url).path != urlparse(url).path:
+        return n_parsClinvar(urlparse(response.url).path.split('/')[-2])
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+
+    f = soup.find(class_="section")
+    if not f:
+        return '[+] clinvar\n\n'+'No results have been found that match your criteria.Please redefine your search criteria.'
+
+
+    hr = f.find_all('td')
+
+    for i in hr:
+        
+        t = i.text.split()
+        if 'item' in t:
+            item = t[2][:-2]
+        try:
+            if re.search(seach,t[0]):
+                n_parsClinvar(item)
+        except IndexError:
+
+            return '[+] clinvar\n\n'+'Too much information specify the request'
+        return '[+] clinvar\n\n'+'No results have been found that match your criteria.Please redefine your search criteria.'
+
+def n_parsClinvar(variation):
+    url = f'https://www.ncbi.nlm.nih.gov/clinvar/variation/{variation}/#id_second'
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    intepr = soup.find_all(class_="bold margin-bottom")
+    mutac = soup.find_all(class_="usa-color-text usa-color-text-white blue-box")
+
+    return '[+] clinvar\n\n'+ mutac[0].text.strip() +'\n'+ intepr[0].text.strip() +'\n' + url
+
 
 
